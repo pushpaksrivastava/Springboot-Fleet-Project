e7c3d1ff129aca060dd55f7c1fe227cd00e6c625
Pushpak Srivastava <pushpaksrivastava01@gmail.com>
2020-12-16 14:49:23 +0530

    First Commit

---
+++ src/main/java/com/example/spring/project/fleetapp/ApplicationController.java
@@ -0,0 +1,12 @@
+package com.example.spring.project.fleetapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+	@GetMapping("/index")
+	public String goHome() {
+		return "index";
+	}
+}
